284c5fa0948ae4373068596b0873cbc0544f7846
madman <suniltejs@gmail.com>
2014-08-30 23:58:39 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GetithomeApp::Application.config.secret_key_base = '71443693b4fa05eea572c4e5cb668de7be6c565b1f579c0fc81efdfecd10e9f1ff65aff40a3e576413fc9a8cde7e6adc59bbfd71d304812d4337420d9cafdca4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GetithomeApp::Application.config.session_store :cookie_store, key: '_getithome_app_session'
